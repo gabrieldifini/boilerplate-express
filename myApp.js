a15f8d5f7dd40c5125d95dca74ffb430ae7194b6
gabrieldifini <gabriel.di.fini@outlook.com>
2022-06-24 23:42:00 -0300

    using static assets from /public

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express()
 
 console.log('Hello World')
 
+app.use("/public", express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
     // res.send('Hello Express')
     const filePath = __dirname + '/views/index.html'
